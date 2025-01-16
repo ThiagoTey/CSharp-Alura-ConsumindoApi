17701093eedb786ec6f330e0bfa36c0121ce4430
Thiago <teyextreme@outlook.com>
2025-01-16 11:02:48 -0300

    Try Catch

--- ScreenSound-4/Program.cs
+++ ScreenSound-4/Program.cs
@@ -1,5 +1,12 @@
 ﻿using (HttpClient client = new HttpClient())
 {
-    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-    Console.Write(resposta);
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+    } catch (Exception ex)
+    {
+        Console.WriteLine($"Aconteceu algo de errado: {ex.Message}");
+    }
+
 }
