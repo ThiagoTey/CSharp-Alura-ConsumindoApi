ee34d61b24d4279252395c6c29780b7001528630
Thiago <teyextreme@outlook.com>
2025-01-17 10:32:17 -0300

    PokemonApi

---
+++ PokeApi/Pokemon/Pokemon.cs
@@ -0,0 +1,22 @@
+﻿namespace PokeApi.Pokemon;
+
+internal class Pokemon
+{
+    public AbilityWrapper[] Abilities { get; set; }
+    public Form[] Forms { get; set; }
+}
+
+public class AbilityWrapper
+{
+    public Ability Ability { get; set; }
+}
+
+public class Ability
+{
+    public string Name { get; set; }
+}
+
+public class Form
+{
+    public string Name { get; set; }
+}
---
+++ PokeApi/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System.Text.Json;
+using PokeApi.Pokemon;
+
+async Task ProcurarPokemon(string nomePokemon)
+{
+    try
+    {
+        using HttpClient client = new HttpClient();
+        string resposta = await client.GetStringAsync($"https://pokeapi.co/api/v2/pokemon/{nomePokemon}");
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true // Ignora diferenças de maiúsculas/minúsculas
+        };
+        Pokemon data = JsonSerializer.Deserialize<Pokemon>(resposta, options);
+
+        if (data == null)
+        {
+            Console.WriteLine("Erro: Não foi possível deserializar o JSON.");
+            return;
+        }
+
+        Console.WriteLine("\nFormas do Pokemom : ");
+        foreach (Form form in data.Forms)
+        {
+            Console.WriteLine(form.Name);
+        }
+
+        Console.WriteLine("\nHabilidades Pokemom : ");
+        foreach (AbilityWrapper ability in data.Abilities)
+        {
+            Console.WriteLine(ability.Ability.Name);
+        }
+
+        Console.WriteLine("\nDigite alguma coisa para continuar");
+        Console.ReadKey();
+        Console.Clear();
+        await ExibirMenu();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Pokemon não encontrado! :" + ex.Message);
+
+        Console.WriteLine("Digite alguma coisa para continuar");
+        Console.ReadKey();
+        await ExibirMenu();
+    }
+}
+
+
+Console.WriteLine("Bem vindo ao PokeApi!");
+async Task ExibirMenu()
+{
+    Console.Write("\nDigite o nome de um pokemon : ");
+    string nomePokemon = Console.ReadLine()!;
+    await ProcurarPokemon(nomePokemon);
+}
+
+await ExibirMenu();
