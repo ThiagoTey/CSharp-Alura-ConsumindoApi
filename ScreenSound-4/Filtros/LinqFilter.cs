0cf3618a4f89f7d89b6316031512d4d26ddd0138
Thiago <teyextreme@outlook.com>
2025-01-18 10:38:06 -0300

    Filtrando generos musicas de uma lista com Linq

---
+++ ScreenSound-4/Filtros/LinqFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using ScreenSound_4.Modelos;
+
+namespace ScreenSound_4.Filtros;
+
+internal class LinqFilter
+{
+    public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+    {
+        var todosOsGenerosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();
+        foreach(var genero in todosOsGenerosMusicais)
+        {
+            Console.WriteLine($" - {genero}");
+        }
+    }
+
+}
