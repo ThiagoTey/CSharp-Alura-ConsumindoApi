0941923bd8caafc5204f4346bfe5297df6b1289d
Thiago <teyextreme@outlook.com>
2025-01-21 12:14:16 -0300

    Atividade 1 DesafioJson

---
+++ DesafioArquivosJson/Modelos/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json;
+
+namespace DesafioArquivosJson.Modelos;
+
+internal class Usuario
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Email { get; set; }
+
+    public Usuario(string nome, int idade, string email)
+    {
+        Nome = nome;
+        Idade = idade;
+        Email = email;
+    }
+
+    public void GerarArquivoJson()
+    {
+        var json = JsonSerializer.Serialize(this);
+
+        string nomeDoArquivo = $"Usuario-{Nome}.json";
+
+        File.WriteAllText(nomeDoArquivo, json);
+        Console.WriteLine($"Arquivo Json Criado com suceso em : {Path.GetFullPath(nomeDoArquivo)}");
+    }
+}
---
+++ DesafioArquivosJson/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using DesafioArquivosJson.Modelos;
+
+void Desafio1()
+{
+    Console.WriteLine("Cadastro de Usuario e criação arquivo json");
+    Console.Write("Insira o nome : ");
+    string nome = Console.ReadLine()!;
+    Console.Write("Insira a idade : ");
+    int idade = int.Parse(Console.ReadLine()!);
+    Console.Write("Insira o Email : ");
+    string email = Console.ReadLine()!;
+
+    Usuario usuario = new(nome, idade, email);
+    usuario.GerarArquivoJson();
+}
+
+Desafio1();
