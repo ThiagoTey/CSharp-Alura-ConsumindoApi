eeabf2e3c6883ee721afa4f71058c43c9f678113
Thiago <teyextreme@outlook.com>
2025-01-21 10:41:50 -0300

    Classe musicas preferidas

---
+++ ScreenSound-4/Modelos/MusicasPreferidas.cs
@@ -0,0 +1,27 @@
+﻿namespace ScreenSound_4.Modelos;
+
+internal class MusicasPreferidas
+{
+    public string? Nome { get; set; }
+    public List<Musica> ListaDeMusicasFavoritas { get; }
+
+    public MusicasPreferidas(string nome)
+    {
+        Nome = nome;
+        ListaDeMusicasFavoritas = new List<Musica>();
+    }
+
+    public void AdicionarMusicasFavoritas(Musica musica)
+    {
+        ListaDeMusicasFavoritas.Add(musica);
+    }
+
+    public void ExibirMusicasFavoritas()
+    {
+        Console.WriteLine($"Musicas favoritas de {Nome}");
+        foreach (Musica musica in ListaDeMusicasFavoritas)
+        {
+            Console.Write($"- {musica.Nome} DE {musica.Artista}");
+        }
+    }
+}
